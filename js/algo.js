360223ca57067968e19321fda77b30c5dca94655
Evan <evansotos@gmail.com>
2017-01-12 14:18:28 -0600

    Fixed Bugs/HTML Throw Errors

--- js/algo.js
+++ js/algo.js
@@ -2,7 +2,7 @@
 {
     //totalPts, yourPts, Weight
 }
-    
+
 function genFields(numCatagories)//Generate # of fields based on how many diferent grading sections they have
 {
 
@@ -12,11 +12,11 @@ function genFields(numCatagories)//Generate # of fields based on how many difere
 //Instead of having it on an Button, have it onBlur Event
 $(document).ready(function (){
     var numCata = 0;
-    var yourPts = []
+    var yourPts = [];
     var totalPts = [];
     var Weight = [];
     var Final = [];
-    
+
     $('#checkbox').click(function(){
         if (this.checked) {
             $('#tab1').animate({left: '-100%'}, 400);
@@ -35,14 +35,14 @@ $(document).ready(function (){
             Final.push((yourPts[i]/totalPts[i])*Weight[i]);
         }
     });
-    
+
     $("#addButton").click(function() {
-        numCata = numCata + 1
-       $("#formPlace").append("<input class='rounded' type='text' placeholder='Your Points' id='"+numCata+"a"+"'>"+" <input class='rounded' type='text' placeholder='Total Points' id='"+numCata+"b"+"'>"+" <input class='rounded' type='text' placeholder='Category Weight' id='"+numCata+"c"+"'>"+"<br id='"+numCata+"d'>")  
-    })
-    
+        numCata = numCata + 1;
+       $("#formPlace").append("<input class='rounded' type='text' placeholder='Your Points' id='"+numCata+"a"+"'>"+" <input class='rounded' type='text' placeholder='Total Points' id='"+numCata+"b"+"'>"+" <input class='rounded' type='text' placeholder='Category Weight' id='"+numCata+"c"+"'>"+"<br id='"+numCata+"d'>");
+    });
+
     $("#subtractButton").click(function() {
-        if(numCata != 0)
+        if(numCata !== 0)
         {
              $("#"+numCata+"a").remove();
              $("#"+numCata+"b").remove();
@@ -51,7 +51,6 @@ $(document).ready(function (){
             numCata = numCata - 1;
         }
     });
-    
+
 });
 //# is id
-
